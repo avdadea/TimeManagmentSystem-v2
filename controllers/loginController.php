e671b4ab91c17d92c790714a532677ac2768bbca
avdadea <avdadea@gmail.com>
2024-01-29 00:26:28 +0100

    ndryshim

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -4,17 +4,21 @@ session_start();
 $msg = "";
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
-    $email = mysqli_real_escape_string($con, $_POST['email']);
-    $password = mysqli_real_escape_string($con, $_POST['password']);
-    $res = mysqli_query($con, "SELECT * FROM employee WHERE email='$email' AND password='$password'");
+    // Dependency injection: Pass $con to the authenticate function
+    authenticate($con, $_POST['email'], $_POST['password']);
+}
+
+function authenticate(mysqli $database, $email, $password)
+{
+    $email = mysqli_real_escape_string($database, $email);
+    $password = mysqli_real_escape_string($database, $password);
+
+    $res = mysqli_query($database, "SELECT * FROM employee WHERE email='$email' AND password='$password'");
     $count = mysqli_num_rows($res);
 
     if ($count > 0) {
         $row = mysqli_fetch_assoc($res);
-        $_SESSION['role'] = $row['role'];
-        $_SESSION['USER_ID'] = $row['employeeid'];
-        $_SESSION['USER_NAME'] = $row['name'];
-        $_SESSION['USER_DEPARTMENT_ID'] = $row['departmentId'];
+        setSession($row);
         header('location:../views/profile.php');
         die();
     } else {
@@ -22,5 +26,13 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
         header('location:../views/login.php?msg=' . $msg);
         die();
     }
+}
+
+function setSession($row)
+{
+    $_SESSION['role'] = $row['role'];
+    $_SESSION['USER_ID'] = $row['employeeid'];
+    $_SESSION['USER_NAME'] = $row['name'];
+    $_SESSION['USER_DEPARTMENT_ID'] = $row['departmentId'];
 }
 ?>
